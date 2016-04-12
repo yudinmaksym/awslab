b0fc589b17a99aae1646659c2cd26123f460b7e3
maksym <4ygakk@gmail.com>
2016-04-12 09:08:36 +0000

    changes

--- example_1.js
+++ example_1.js
@@ -1,8 +1,23 @@
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./config.json');
+
 var task =  function(request, callback){
 
 	var fName = request.query.fName ? request.query.fName : "missing parameter: fName";
 	var lName = request.query.lName ? request.query.lName : "missing parameter: lName";
-	callback(null, fName + " " + lName);
+
+	var ec2 = new AWS.EC2();
+
+	ec2.describeInstances({}, function(err, data) {
+	  if (err) console.log(err, err.stack); // an error occurred
+	  else {     
+		console.log(data);           // successful response
+		callback(null, data);
+	}
+
+
+	});
+	
 }
 
 exports.lab = task
